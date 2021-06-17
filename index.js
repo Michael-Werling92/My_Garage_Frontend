7236d2dc442b72b6fa32214eac774cf82ef4ac25
Michael <mfw6mw@virginia.edu>
2021-06-17 16:00:29 -0400

    added new car addition functionality

--- index.js
+++ index.js
@@ -4,5 +4,30 @@ document.addEventListener("DOMContentLoaded", function(){  console.log("DOM IS L
 
     API.fetchAllCars()
 
-
+    const newToyForm = document.querySelector(".add-car-form")
+        newToyForm.addEventListener("submit", event =>{ event.preventDefault(); 
+          const year = event.target.year.value
+          const make = event.target.make.value
+          const model = event.target.model.value
+          const color = event.target.color.value
+          const image = event.target.image.value
+            const submit = event.target.submit
+            console.log("YAYYYYYYYYYYY", submit)
+            
+            fetch("http://localhost:3000/cars", {
+        
+                method: "POST",
+                headers: { "Content-Type": "application/json"},
+                body: JSON.stringify({
+    
+                      "year": year,
+                      "make": make,
+                      "model": model,
+                      "color": color,
+                      "image": image
+                }
+              ,)})
+              .then(response => response.json())
+              .then(theThingWePosted => console.log("Info:", theThingWePosted))
+      })
 })
