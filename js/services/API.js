cef97e54fce840801f195dc62c7e2a5067c2b00d
Michael <mfw6mw@virginia.edu>
2021-06-17 15:32:08 -0400

    added new button javascript to index js

--- js/services/API.js
+++ js/services/API.js
@@ -2,7 +2,8 @@ class API{
 
     static fetchAllCars(){
 
-        fetch("http://localhost:3000/cars").then(response => response.json()).then(fetchedArray => fetchedArray.forEach
+        fetch("http://localhost:3000/cars").then(response => response.json())
+        .then(fetchedArray => fetchedArray.forEach
             (car => {console.log(car)
             const newCar = new Car(car)
             newCar.renderCar(car)
