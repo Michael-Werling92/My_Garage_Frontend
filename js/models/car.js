1b160679dc3decab186b40e810d373d8c4a4dc65
Michael <mfw6mw@virginia.edu>
2021-06-28 14:30:52 -0400

    allowed users to edit car appearnace

--- js/models/car.js
+++ js/models/car.js
@@ -21,8 +21,9 @@ class Car{
         
         return `
             <h2 id="car.id" data-id="${this.id}">${this.year} ${this.make} ${this.model}</h2>
-            <img src="https://cdn3.iconfinder.com/data/icons/car-icons-front-views/480/Generic_Car_Front_View-512.png" class="car-avatar" /><br></br>
+            <img src="${this.image}" class="car-avatar" /><br></br>
             <h3>Color: ${this.color}<br></br> VIN: ${this.vin}</h3>
+            <button data-id="${this.id}" class="edit-btn"> EDIT CAR INFO </button>
             <button data-id="${this.id}" class="maintenance-btn"> VIEW MAINTENANCE </button>
             <button data-id="${this.id}" class="delete-btn"> DELETE CAR FROM GARAGE </button>
             <div class="card" event-id="${this.id}"></div>
@@ -46,6 +47,85 @@ class Car{
             const id = event.target.dataset.id
             API.fetchMyRecords(id)
           }
-        })
-    }
-}
+          if(event.target.matches(".edit-btn")){
+            const id = event.target.dataset.id
+            const cardEditing = document.querySelector(`.card[event-id="${id}"]`)
+            console.log(cardEditing)
+            const editCarForm = document.createElement("form")
+            editCarForm.innerHTML =`
+                    <br><br>
+                    <h2>Edit This Car:</h2>
+                        <form class="name-change-form">
+                        <br>
+                        <button class="close-button">✖️CLOSE</button>
+                        <br>
+
+                          <br />
+                        <h4>New Color:</h4>
+                        <input
+                        type="text"
+                        name="color"
+                        value=""
+                        placeholder=""
+                        class="input-car-color"
+                          />
+
+
+                        <h4>Image URL:</h4>
+                        <input
+                        type="text"
+                        name="image"
+                        value=""
+                        placeholder=""
+                        class="input-text-image"
+                        />        
+                          <br />
+                        <input
+                        type="submit"
+                        name="submit"
+                        value="Update Car Information"
+                        class="submit-button"
+                        />
+                        </form>
+                        <br><br><br><br>
+                        `
+                    cardEditing.append(editCarForm)
+
+            const closeButton = editCarForm.querySelector(".close-button")
+                closeButton.addEventListener("click", (event)=>{
+                        editCarForm.remove()
+                        })
+
+            editCarForm.addEventListener("click", (event)=>{  event.preventDefault();
+                        if(event.target.matches(".submit-button")){
+
+                          let updatedColor = editCarForm.querySelector(".input-car-color").value
+                              console.log(updatedColor)
+                          let updatedImage = editCarForm.querySelector(".input-text-image").value
+                              console.log(updatedImage)
+
+                          const bodyObj = {
+
+                            color: updatedColor,
+                            image: updatedImage 
+
+                          }
+                          fetch(`http://localhost:3000/cars/${id}`,{
+                            method: "PATCH",
+                            headers: { "Content-Type": "application/json" },
+                            body: JSON.stringify(bodyObj),
+                          })
+                          .then(r => r.json())
+                          .then(updatedCar => {  
+                              console.log("", updatedCar)
+
+                              cardEditing.color = updatedColor.color
+                              cardEditing.image = updatedImage.image
+                          })
+                        }
+
+
+                    })
+            }
+        }
+    )}}
